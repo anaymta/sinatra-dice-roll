19c9b4a1c0551607c3a6044388ea559fd907c2d7
anaymta <74467992+anaymta@users.noreply.github.com>
2023-10-17 00:34:45 +0000

    8

--- dice.rb
+++ dice.rb
@@ -19,53 +19,61 @@ get("/giraffe") do
   "Hopefully this shows up without having to restart the server 🤞🏾"
 end
 get("/dice/2/6") do
-  first_die = rand(1..6)
-	second_die = rand(1..6)
-  sum = first_die + second_die
+  @rolls = []
 
-	outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
-	@outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
+  2.times do
+    die = rand(1..6)
 
-	"<h1>2d6</h1>
-   <p>#{outcome}</p>"
-   erb(:two_six)
+    @rolls.push(die)
+  end
+
+  erb(:two_six)
   end
 
 get("/dice/2/10") do
-  first_die = rand(1..10)
-	second_die = rand(1..10)
-  sum = first_die + second_die
+  @rolls = []
 
-	outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
-	@outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
+  2.times do
+    die = rand(1..10)
 
-	"<h1>2d10</h1>
-   <p>#{outcome}</p>"
+    @rolls.push(die)
+  end
+
   erb(:two_ten)
 end
 
 get("/dice/1/20") do
-  die = rand(1..20)
+  @rolls = []
 
-	outcome = "You rolled a #{die}."
-	@outcome = "You rolled a #{die}."
+  1.times do
+    die = rand(1..20)
 
-	"<h1>1d20</h1>
-   <p>#{outcome}</p>"
-	erb(:one_twenty)
+    @rolls.push(die)
+  end
+
+  erb(:one_twenty)
 end
 
 get("/dice/5/4") do
-  first_die = rand(1..4)
-	second_die = rand(1..4)
-  third_die = rand(1..4)
-  fourth_die = rand(1..4)
-  sum = first_die + second_die + third_die + fourth_die
+  @rolls = []
 
-	outcome = "You rolled a #{first_die}, a #{second_die}, a #{third_die}, and a #{fourth_die} for a total of #{sum}."
-	@outcome = "You rolled a #{first_die}, a #{second_die}, a #{third_die}, and a #{fourth_die} for a total of #{sum}."
+  5.times do
+    die = rand(1..4)
 
-	"<h1>5d4</h1>
-   <p>#{outcome}</p>"
-  erb(:five_four)
+    @rolls.push(die)
+  end
+
+  erb(:five_four )
+end
+
+get("/dice/100/6") do
+  @rolls = []
+
+  100.times do
+    die = rand(1..6)
+
+    @rolls.push(die)
+  end
+
+  erb(:one_hundred_six)
 end
